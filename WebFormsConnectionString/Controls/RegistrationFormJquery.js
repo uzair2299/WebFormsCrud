d6b1d2fc82b2a630fd3f28689484b369fb451b61
uzair <uzairanwar2299@gmail.com>
2020-10-22 01:35:17 +0500

    JqueryForm

---
+++ WebFormsConnectionString/Controls/RegistrationFormJquery.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    var item = {};
+    $("#RegistrationFormJquery").find('input[type="text"],input[type="password"],input[type="email"]').each(function () {
+        item[this.id] = $(this).val();
+    });
+    console.log(item);
+});
